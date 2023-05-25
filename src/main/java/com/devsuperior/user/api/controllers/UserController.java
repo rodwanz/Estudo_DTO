31e6370b0a1bdbf7e730d6b84327e9d7c486dec2
Rodrigo Wanzeler <wanzervai@gmail.com>
2023-05-25 12:17:39 -0300

    Usando Modelo de Representação(DTO) para substituir as entidades

--- src/main/java/com/devsuperior/user/api/controllers/UserController.java
+++ src/main/java/com/devsuperior/user/api/controllers/UserController.java
@@ -1,10 +1,10 @@
 package com.devsuperior.user.api.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.user.api.dto.UserDTO;
 import com.devsuperior.user.domain.model.User;
 import com.devsuperior.user.domain.repositories.UserRepository;
 import com.devsuperior.user.domain.services.UserService;
@@ -28,18 +29,36 @@ public class UserController {
 	private UserRepository userRepository;
 	
 	@GetMapping
-	public List<User> listing(){
-		return userRepository.findAll(); 
+	public List<UserDTO> listing(){
+		return toCollectionDTO(userRepository.findAll()); 
 	}
 	
 	@GetMapping("/{id}")
-	public User findById(@PathVariable Long id) {
-		return userService.searchingForId(id);
+	public UserDTO findById(@PathVariable Long id) {
+		User user = userService.searchingForId(id);		
+		return toDTO(user);
 	}
 		
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public User adding(@RequestBody User user) {
-		return userService.saving(user);
+	public UserDTO adding(@RequestBody User user) {
+		return toDTO(userService.saving(user));
+	}
+	
+
+	private UserDTO toDTO(User user) {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setId(user.getId());
+		userDTO.setName(user.getName());
+		userDTO.setEmail(user.getEmail());
+		userDTO.setPhone(user.getPhone());
+		userDTO.setDocment(user.getDocument());
+		return userDTO;
+	}
+	
+	private List<UserDTO> toCollectionDTO(List<User> users){
+		return users.stream()
+				.map(user -> toDTO(user))
+				.collect(Collectors.toList());
 	}
 }
