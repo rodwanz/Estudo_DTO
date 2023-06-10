7ff7e3b02c2e1f203f3b6fed82e436bd69065b12
Rodrigo Wanzeler <wanzervai@gmail.com>
2023-06-09 21:04:49 -0300

    Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/devsuperior/user/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/devsuperior/user/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package com.devsuperior.user.api.exceptionhandler;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -14,43 +15,66 @@ import com.devsuperior.user.domain.exception.UserNotFound;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		ProblemType problemType = ProblemType.UNDEFINED_MESSAGE;
+		String detail = "The request body is invalid. Check syntax error";
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
+				status, request);
+	}
 
 	@ExceptionHandler(EntityNotFound.class)
 	public ResponseEntity<?> dealWithEntityNotFound(
 			EntityNotFound ex, WebRequest request) {
 		
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		ProblemType problemType = ProblemType.ENTITY_NOT_FOUND;
+		String detail = ex.getMessage();
 		
-		Problem problem = Problem.builder()
-				.status(status.value())
-				.title("Entity not found")
-				.detail(ex.getMessage())
-				.build();
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
 
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
 				status, request);
 	}
 
 	@ExceptionHandler(UserNotFound.class)
-	public ResponseEntity<?> dealWithUserNotFound(UserNotFound ex, WebRequest request) {
+	public ResponseEntity<?> dealWithUserNotFound(UserNotFound ex, 
+			WebRequest request) {
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ProblemType problemType = ProblemType.USER_NOT_FOUND;
+		String detail = ex.getMessage();
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
 
-		return handleExceptionInternal(ex, 
-				ex.getMessage(), 
-				new HttpHeaders(), 
-				HttpStatus.BAD_REQUEST, request);
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
+				status, request);
+
 	}
 
 	@ExceptionHandler(DataInUse.class)
-	public ResponseEntity<?> dealWithDataInUse(DataInUse ex, WebRequest request) {
+	public ResponseEntity<?> dealWithDataInUse(DataInUse ex, 
+			WebRequest request) {
+		
+		HttpStatus status = HttpStatus.CONFLICT;
+		ProblemType problemType = ProblemType.DATA_IN_USE;
+		String detail = ex.getMessage();
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
 
-		return handleExceptionInternal(ex, 
-				ex.getMessage(), 
-				new HttpHeaders(), 
-				HttpStatus.CONFLICT, request);
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
+				status, request);
 	}
 
 	@Override
-	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, 
+			Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
 		if (body == null) {
 			body = Problem.builder()
@@ -64,5 +88,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 					.build();
 		}
 		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+	
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, 
+			ProblemType problemType, String detail){
+		
+		return Problem.builder()
+				.status(status.value())
+				.title(problemType.getTitle())
+				.detail(detail);
 	}
 }
